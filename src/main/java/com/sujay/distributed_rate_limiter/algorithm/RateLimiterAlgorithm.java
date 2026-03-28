7c8b0b21e24b066f431726bcd72101ac1cb384d3
sujay-777 <sujaysudharshan7@gmail.com>
2026-03-28 23:11:11 +0530

    Removed/deleted the testing temp files

--- src/main/java/com/sujay/distributed_rate_limiter/algorithm/RateLimiterAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.sujay.distributed_rate_limiter.algorithm;
-
-import com.sujay.distributed_rate_limiter.model.RateLimitRequest;
-import com.sujay.distributed_rate_limiter.model.RateLimitResponse;
-
-public interface RateLimiterAlgorithm {
-
-    RateLimitResponse check(RateLimitRequest request);
-}
